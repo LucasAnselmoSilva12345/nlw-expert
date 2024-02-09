b72a1df98ac36cada70876323607f22a976a383f
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-02-09 20:08:25 -0300

    feat: Font Family Inter
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from './mode-toggle';
 
 export function Header() {
   return (
-    <header className="w-full my-0 mx-auto pt-4 text-center flex items-center justify-center">
+    <header className="w-full mx-auto p-6 xl:px-0 xl:max-w-7xl flex items-center justify-between">
       <img src={logoNLWExpertNotes} alt="Nlw logo" />
       <ModeToggle />
     </header>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,12 @@ import { NoteCard } from '../components/NoteCard';
 
 export function Home() {
   return (
-    <section className="mx-auto w-full p-6 xl:max-w-7xl xl:p-0 my-12 space-y-6">
+    <section className="mx-auto w-full p-6 xl:max-w-7xl xl:p-0 xl:my-6 space-y-6">
       <form className="w-full">
         <input
           type="text"
           placeholder="Search your notes"
-          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500 transition-all duration-200 focus-visible:ring-1 focus-visible:ring-slate-600"
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight dark:font-medium outline-none placeholder:text-slate-500 transition-all duration-200 focus-visible:ring-1 focus-visible:ring-slate-600"
         />
       </form>
 
