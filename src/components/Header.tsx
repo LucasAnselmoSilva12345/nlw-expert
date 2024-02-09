3432068181e623d4993da1ee9b6f56397dc1fc3c
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-02-09 19:51:23 -0300

    feat: Add Mode Toggle switch theme
    This commit add switch theme with ShadcnUI properties and it's can switch theme with three options: Light, Dark and theme system
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import logoNLWExpertNotes from '../assets/logo-nlw-expert.svg';
+import { ModeToggle } from './mode-toggle';
 
 export function Header() {
   return (
     <header className="w-full my-0 mx-auto pt-4 text-center flex items-center justify-center">
       <img src={logoNLWExpertNotes} alt="Nlw logo" />
+      <ModeToggle />
     </header>
   );
 }
