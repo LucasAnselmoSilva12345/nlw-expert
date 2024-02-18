1bb318588eaee0a000a9aac3f5cd023e7adcdf1c
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-02-18 14:11:11 -0300

    feat: Search notes
    This commit add the function about the search notes via text
    
    Issue: https://github.com/LucasAnselmoSilva12345/nlw-expert/issues/3
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { NewNoteCard } from '../components/NewNoteCard';
 import { NoteCard } from '../components/NoteCard';
 
@@ -9,6 +9,8 @@ interface NotesProps {
 }
 
 export function Home() {
+  const [search, setSearch] = useState('');
+
   const [notes, setNotes] = useState<NotesProps[]>(() => {
     const notesOnStorage = localStorage.getItem('notes');
 
@@ -33,6 +35,18 @@ export function Home() {
     localStorage.setItem('notes', JSON.stringify(notesArray));
   }
 
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+    setSearch(query);
+  }
+
+  const filteredNotes =
+    search !== ''
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )
+      : notes;
+
   return (
     <section className="mx-auto w-full p-6 xl:max-w-7xl xl:p-0 xl:my-6 space-y-6">
       <form className="w-full">
@@ -40,6 +54,7 @@ export function Home() {
           type="text"
           placeholder="Search your notes"
           className="w-full bg-transparent text-3xl text-slate-700 dark:text-slate-300 font-semibold tracking-tight dark:font-medium outline-none placeholder:text-slate-700 dark:placeholder:text-slate-500 transition-all duration-200 focus-visible:ring-1 focus-visible:ring-slate-600"
+          onChange={handleSearch}
         />
       </form>
 
@@ -48,7 +63,7 @@ export function Home() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        {notes.map((noteProps) => {
+        {filteredNotes.map((noteProps) => {
           return <NoteCard key={noteProps.id} note={noteProps} />;
         })}
       </div>
